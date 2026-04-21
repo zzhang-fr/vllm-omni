def46ff2a07e7e32d9393a7959ee10164f4edf23
Mahdi Nasser <94046147+mnasser02@users.noreply.github.com>
2026-04-21 09:01:29 +0200

    Add stream_batch arg
    Signed-off-by: Mahdi Nasser <94046147+mnasser02@users.noreply.github.com>

--- vllm_omni/engine/async_omni_engine.py
+++ vllm_omni/engine/async_omni_engine.py
@@ -1272,6 +1272,7 @@ class AsyncOmniEngine:
             "parallel_config": parallel_config,
             "model_class_name": kwargs.get("model_class_name", None),
             "step_execution": kwargs.get("step_execution", False),
+            "stream_batch": kwargs.get("stream_batch", False),
             "vae_use_slicing": kwargs.get("vae_use_slicing", False),
             "vae_use_tiling": kwargs.get("vae_use_tiling", False),
             "cache_backend": cache_backend,
